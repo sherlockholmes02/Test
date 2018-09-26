c2709d115c015df5fbc1533e5f3c0099b2b7baa5
Deocales Karl Andrew <citmobiledev01@gmail.com>
2018-09-26 11:02:37 +0800

    Update and Delete Note -De Castro, Darryl Dave P.

--- app/src/main/java/com/dave/test/MainActivity.java
+++ app/src/main/java/com/dave/test/MainActivity.java
@@ -1,12 +1,15 @@
 package com.dave.test;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import java.util.List;
 
@@ -14,14 +17,26 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private FloatingActionButton addNote;
+    private RelativeLayout parent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        parent = findViewById(R.id.parent);
         recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         addNote = findViewById(R.id.addNote);
 
+        Intent intent = getIntent();
+
+        if(intent.getBooleanExtra("isSaved", false)) {
+            Util.snackToast(parent, "Note Saved!", Util.SNACK_TOAST_SUCCESS);
+        }else if(intent.getBooleanExtra("isDeleted", false)){
+            Util.snackToast(parent, "Note Deleted!", Util.SNACK_TOAST_SUCCESS);
+        }
+
+        getNotes();
         sendToAdd();
     }
 
@@ -33,6 +48,32 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void getNotes(){
+
+        class GetANotes extends AsyncTask<Void, Void, List<Note>>{
+
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                List<Note> notelist = DatabaseClient.getInstance(getApplicationContext()).getAppDatabase().noteDao().getAll();
+                return notelist;
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> noteList) {
+                super.onPostExecute(noteList);
+
+                NoteAdapter adapter = new NoteAdapter(MainActivity.this, noteList);
+                recyclerView.setAdapter(adapter);
+
+            }
+        }
+
+        GetANotes gn = new GetANotes();
+        gn.execute();
+
+
     }
 
 
