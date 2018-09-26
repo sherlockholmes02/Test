e971bfd34572cc347a688dd58bb089eba4ddf2c6
Deocales Karl Andrew <citmobiledev01@gmail.com>
2018-09-26 09:18:25 +0800

    Add Note -De Castro, Darryl Dave P.

--- app/src/main/java/com/dave/test/MainActivity.java
+++ app/src/main/java/com/dave/test/MainActivity.java
@@ -1,13 +1,41 @@
 package com.dave.test;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private FloatingActionButton addNote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        addNote = findViewById(R.id.addNote);
+
+        sendToAdd();
     }
+
+    private void sendToAdd(){
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddNoteActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+
 }
